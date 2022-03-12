4336008c423ffda965b9aaabab3be03c8d854928
Alexey Minnekhanov <alexeymin@postmarketos.org>
2022-03-12 20:55:10 +0300

    WIP: add xiaomi-lavender board

--- arch/arm/mach-snapdragon/sysmap-sdm660.c
+++ arch/arm/mach-snapdragon/sysmap-sdm660.c
@@ -8,12 +8,15 @@
 #include <common.h>
 #include <asm/armv8/mmu.h>
 
-/* From stock bootloader output:
-* Add Base: 0x0000000040000000 Available Length: 0x0000000060000000 
-* Add Base: 0x00000000A0000000 Available Length: 0x000000005EAC0000
-*/
+#if CONFIG_IS_ENABLED(TARGET_XIAOMI_LAVENDER)
 
-static struct mm_region sdm660_mem_map[] = {
+/* From stock bootloader output:
+ * Add Base: 0x0000000040000000 Available Length: 0x0000000060000000
+ * Add Base: 0x00000000A0000000 Available Length: 0x000000005EAC0000
+ * 0x60000000 + 0x5eac0000 = 0xbeac0000
+ */
+
+static struct mm_region sdm660_lavender_mem_map[] = {
 	{
 		.virt = 0x0UL, /* Peripheral block */
 		.phys = 0x0UL, /* Peripheral block */
@@ -22,17 +25,9 @@ static struct mm_region sdm660_mem_map[] = {
 			 PTE_BLOCK_NON_SHARE |
 			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
 	}, {
-		/* 1st DDR bank ~1.5 Gb */
-		.virt = 0x40000000UL, /* DDR */
-		.phys = 0x40000000UL, /* DDR */
-		.size = 0x60000000UL,
-		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
-			 PTE_BLOCK_INNER_SHARE
-	}, {
-		/* 2nd DDR bank ~1.5 Gb */
-		.virt = 0xA0000000UL, /* DDR */
-		.phys = 0xA0000000UL, /* DDR */
-		.size = 0x5EAC0000UL,
+		.virt = 0x40000000UL, /* DDR start */
+		.phys = 0x40000000UL, /* DDR start */
+		.size = 0xbeac0000UL,
 		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
 			 PTE_BLOCK_INNER_SHARE
 	}, {
@@ -41,4 +36,6 @@ static struct mm_region sdm660_mem_map[] = {
 	}
 };
 
-struct mm_region *mem_map = sdm660_mem_map;
+struct mm_region *mem_map = sdm660_lavender_mem_map;
+
+#endif
